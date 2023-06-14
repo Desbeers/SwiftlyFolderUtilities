0c369c00563de26e54759f1aade7bd97321abe2b
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2023-06-14 18:44:24 +0200

    Add bookmark and monitor functions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftlyFolderUtilities",
+    platforms: [.macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,8 +22,5 @@ let package = Package(
         .target(
             name: "SwiftlyFolderUtilities",
             dependencies: []),
-        .testTarget(
-            name: "SwiftlyFolderUtilitiesTests",
-            dependencies: ["SwiftlyFolderUtilities"]),
     ]
 )
--- Sources/SwiftlyFolderUtilities/SwiftlyFolderUtilities.swift
+++
@@ -1,6 +0,0 @@
-public struct SwiftlyFolderUtilities {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
